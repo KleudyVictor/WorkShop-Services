6f558bdd6676778fbe5a62cab3977f2b1e7e93b7
Kleudy Victor <97324956+KleudyVictor@users.noreply.github.com>
2022-09-04 01:30:37 -0400

    qr

--- src/pages/PagoPage.vue
+++ src/pages/PagoPage.vue
@@ -2,35 +2,46 @@
   <div class="q-pa-md fit row wrap justify-center items-start content-start">
     <q-card class="my-card text-center text-bold">
       <q-card-section>
-      <q-img  width="300px" height="300px"  src="../../qr-50.jpg" />
-      <div class="q-pa-md q-mx-auto" style="max-width: 330px">
-        <q-input standout v-model="codigo_trans" label="Codigo de Transaccion" />
-      </div>
+        <q-img width="300px" height="300px" src="../assets/qr/1.jpg" />
+        <div class="q-pa-md">
+          <h6>Cantidad de Cuentas: 5</h6>
+          <h6>Monto Total: $ 75</h6>
+        </div>
+        <div class="q-pa-md q-mx-auto" style="max-width: 330px">
+          <q-input
+            standout
+            v-model="codigo_trans"
+            label="Codigo de Transaccion"
+          />
+        </div>
       </q-card-section>
     </q-card>
     <div class="q-pa-md">
-      <q-btn outline style="color: goldenrod;" label="Confirmar Pago" to="/pago"/>
+      <q-btn
+        outline
+        style="color: goldenrod"
+        label="Confirmar Pago"
+        to="/pago"
+      />
     </div>
   </div>
-
-
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
-import { LocalStorage } from 'quasar'
+import { defineComponent, ref } from 'vue';
+import { LocalStorage } from 'quasar';
 
 export default defineComponent({
-    setup() {
-      const listaCorreos = LocalStorage.getItem('listaCorreos') || []
-      const codigo_trans = ref('')
+  setup() {
+    const listaCorreos = LocalStorage.getItem('listaCorreos') || [];
+    const codigo_trans = ref('');
 
-      return {
-        listaCorreos,
-        codigo_trans
-      }
-    },
-})
+    return {
+      listaCorreos,
+      codigo_trans,
+    };
+  },
+});
 </script>
 
 <style lang="sass" scoped>
