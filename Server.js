dde2fd82048cdfaeea521ae4fddb6987864c3d00
michelapm <79293462+michelapm@users.noreply.github.com>
2022-09-05 01:26:58 -0400

    update

--- Server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const serveStatic = require("serve-static");
-const history = require('connect-history-api-fallback');
-const app = express();
-
-app.use(history());
-app.use(serveStatic(__dirname + '/dist/spa'));
-app.listen(port)
