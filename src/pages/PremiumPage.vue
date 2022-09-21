d8e847a84d2ce013b48f0174df8e389a6d4bf721
michelapm <79293462+michelapm@users.noreply.github.com>
2022-09-20 22:27:29 -0400

    update

--- src/pages/PremiumPage.vue
+++ src/pages/PremiumPage.vue
@@ -96,7 +96,7 @@ export default defineComponent({
         );
         cantidadDisponible.value = response.data.cantidad_pedidos;
         LocalStorage.set('cantidadDisponible', cantidadDisponible.value);
-      }, 2000);
+      }, 60000);
       LocalStorage.clear();
     }
     );
